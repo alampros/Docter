0e50a59d7695d048e62b063841c01035f7686b0e
Aaron Lampros <alampros@dealertire.com>
2011-07-18 20:36:17 -0400

    Added Marked.app stylesheet. Removed deps.

--- docter.js
+++ docter.js
@@ -1,15 +1,19 @@
 #!/usr/local/bin/node
 var exec = require('child_process').exec,
 	spawn = require('child_process').spawn,
-	gfm = spawn('/Projects/docter/bin/github-flavored-markdown.rb'),
-	syncbuffer = '';
-
+	// gfm = spawn('./bin/github-flavored-markdown.rb'),
+	gfm = spawn('/usr/local/bin/gfm');
+	
 	gfm.stdout.on('data', function(data) {
-		syncbuffer += data.toString();
+		process.stdout.write(data);
 	});
 	gfm.on('exit',function(ecode){
-		process.stdout.end(syncbuffer,'utf8');
+		
 	})
+
+process.stderr.on('data',function(err) {
+	process.stdout.write('ERR: '+err);
+})
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
