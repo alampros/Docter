f356690cbfeee899641ae830c9feadb9332f5064
Aaron Lampros <alampros@gmail.com>
2011-07-18 20:00:37 -0400

    Initial version

---
+++ docter.js
@@ -0,0 +1,24 @@
+#!/usr/local/bin/node
+var exec = require('child_process').exec,
+	spawn = require('child_process').spawn,
+	// gfm = spawn('./bin/github-flavored-markdown.rb');
+	gfm = spawn('/usr/local/bin/gfm');
+	
+	gfm.stdout.on('data', function(data) {
+		process.stdout.write(data);
+	});
+	gfm.on('exit',function(ecode){
+		
+	})
+
+process.stderr.on('data',function(err) {
+	process.stdout.write('ERR: '+err);
+})
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function (chunk) {
+	gfm.stdin.write(chunk);
+	gfm.stdin.end();
+});
