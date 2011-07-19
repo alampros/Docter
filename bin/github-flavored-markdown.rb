5dd99fb0aeadad73a7293064903e3d512d5e9663
Aaron Lampros <alampros@dealertire.com>
2011-07-18 21:18:43 -0400

    Updated styles to more closely match github\'s

--- bin/github-flavored-markdown.rb
+++ bin/github-flavored-markdown.rb
@@ -20,7 +20,7 @@ require 'albino'
 require 'nokogiri'
 
 def markdown(text)
-  options = [:fenced_code,:generate_toc,:hard_wrap,:no_intraemphasis,:strikethrough,:gh_blockcode,:autolink]
+  options = [:fenced_code,:generate_toc,:hard_wrap,:no_intraemphasis,:strikethrough,:gh_blockcode,:autolink,:xhtml]
   html = Redcarpet.new(text, *options).to_html 
   syntax_highlighter(html)
 end
