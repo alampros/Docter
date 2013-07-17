fb326f8a0581fd71f4464341b498930e58ca14a8
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-07-17 14:12:42 +0200

    Use "/usr/bin/env node" to be system independent

--- docter.js
+++ docter.js
@@ -1,14 +1,14 @@
-#!/usr/local/bin/node
+#!/usr/bin/env node
 var exec = require('child_process').exec,
 	spawn = require('child_process').spawn,
 	// gfm = spawn('./bin/github-flavored-markdown.rb');
 	gfm = spawn('/usr/local/bin/gfm');
-	
+
 	gfm.stdout.on('data', function(data) {
 		process.stdout.write(data);
 	});
 	gfm.on('exit',function(ecode){
-		
+
 	})
 
 process.stderr.on('data',function(err) {
